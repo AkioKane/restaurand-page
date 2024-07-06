0cb0b92997f7939cfebe9b97f9dc38a89808f200
Akio <aakocherga@mail.ru>
2024-07-07 00:29:42 +0700

    [Add] menu

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import './style.css';
-import { Load } from './load.js'
+import { Load } from './load.js';
 
-const content = document.getElementById("content")
+const home = document.getElementById("home")
+const menu = document.getElementById("menu")
+const about = document.getElementById("about")
+const btnMenu = document.querySelector(".btn-menu")
 
 function start() {
-  Load.loadContent()
+  Load.loadHome();
+
+  home.addEventListener("click", () => {
+    Load.loadHome();
+  });
+  menu.addEventListener("click", () => {
+    Load.loadMenu();
+  });
+  
 }
 
 start()
--- src/load.js
+++ src/load.js
@@ -1,9 +1,38 @@
+const content = document.getElementById("content")
+
 export class Load {
   constructor() {
-    // ...
+    // initialization variables
   }
 
-  static loadContent() {
+  static clear() {
+    content.innerHTML = ""
+  }
+
+  static item(elementChild, name, text, ammout) {
+    let item = document.createElement("div")
+    item.classList.add("item")
+    elementChild.appendChild(item)
+
+    let head = document.createElement("h1")
+    head.innerHTML = name
+    item.appendChild(head)
+
+    let p = document.createElement("p")
+    p.innerHTML = text
+    item.appendChild(p)
+
+    let cost = document.createElement("h1")
+    cost.innerHTML = ammout + "$"
+    item.appendChild(cost)
+
+    let hr = document.createElement("hr")
+    item.appendChild(hr)
+  }
+
+  static loadHome() {
+    this.clear()
+
     let container = document.createElement("div")
     container.classList.add("container")
     content.appendChild(container)
@@ -23,5 +52,32 @@ export class Load {
     btn.classList.add("btn-menu")
     btn.innerHTML = "MENU"
     container.appendChild(btn)
+
+    btn.addEventListener("click", () => {
+      this.loadMenu()
+    })
+  }
+
+  static loadMenu() {
+    this.clear()
+
+    let menuContainer = document.createElement("div")
+    menuContainer.classList.add("menu-container")
+    content.appendChild(menuContainer)
+
+    let container = document.createElement("div")
+    container.classList.add("container")
+    menuContainer.appendChild(container)
+
+    let h1 = document.createElement("h1")
+    h1.innerHTML = "MENU"
+    container.appendChild(h1)
+    
+    let hr = document.createElement("hr")
+    container.appendChild(hr)
+
+    this.item(container, "Peperoni", "Сreamy mozzarella cheese and delicate tomato sauce, as well as spicy sausages.", 20)
+    this.item(container, "Margarita", "Сlassic Italian pizza, best tomato.", 15)
+    this.item(container, "Cesadilia", "Chicken, Chicken", 30)
   }
 }
